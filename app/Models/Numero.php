a233ec76c4597c0ebb136627ff53ce5142905980
fulgore93 <50746758+Fulgore93@users.noreply.github.com>
2023-06-28 16:09:16 -0400

    modelo

---
+++ app/Models/Numero.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Numero extends Model
+{
+    use HasFactory;
+
+    protected $table = 'numeros';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'numero_string1',
+        'numero_string2',
+        'numero_float1',
+        'numero_float2',
+        'numero_int1',
+        'numero_int2',
+        'numero_double1',
+        'numero_double2',
+    ];
+}
